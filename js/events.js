906ec789243af77d587ee5499968372ba3dac44d
nil <xetaxe@gmail.com>
2022-06-11 15:03:44 +0200

    game options + table update

---
+++ js/events.js
@@ -0,0 +1,30 @@
+var game;
+
+$(document).ready(function() {
+	$("#game_form").submit( function(){
+
+		const players = $("#players").val();
+		const rounds = $("#rounds").val();
+
+		if (players == null || players == NaN || players < 2 || players > 15) {
+			$("#test").text("Invalid number of players");
+			return false;
+		}
+		if (rounds == NaN || rounds < 2 || rounds > 15) {
+			$("#test").text("Invalid number of rounds");
+			return false;
+		}
+
+		game = new Game(players, rounds);
+		game.createPlayers();
+		game.displayPlayers();
+		$("#create_game").val("Redefine Game");
+		$("#start_game").show();
+
+
+		// $("#test").text(JSON.stringify(game, undefined, 2))
+
+		return false;
+	});
+
+});
--- js/game.js
+++ js/game.js
@@ -2,7 +2,6 @@
 
 const AVAILABLE_NAMES = ["Jonathan", "Sophie", "Jessica", "Nathan", "John", "Stuart", "Jimmy", "Robert", "Eric", "Leonora", "Carmen", "Andrew", "Leena", "Jane", "Wolfgang"];
 
-
 class Game {
 	constructor(numPlayers, numRounds) {
 		this.numPlayers = numPlayers;
@@ -19,7 +18,7 @@ class Game {
 		let playersNames = [];
 
 		do{
-			let indexName = AVAILABLE_NAMES[Math.floor(Math.random * AVAILABLE_NAMES.length)];
+			let indexName = AVAILABLE_NAMES[Math.floor(Math.random() * AVAILABLE_NAMES.length)];
 			if (playersNames.includes(indexName) == false){
 				playersNames.push(indexName);
 			}
@@ -33,9 +32,17 @@ class Game {
 			let newPlayer = new Player(playersNames[i]);
 			this.activePlayers.push(newPlayer);
 		}
+
+		return;
+	}
+
+	displayPlayers(){
+		if (this.activePlayers.length == 0) {return false;}
+
+		for (let player of this.activePlayers){
+			$("#game_table").append("<tr><td>" + player.name + "</td><td>" + player.score + "</td></tr>");
+		}
+
+		return;
 	}
 }
-
-var newGame = new Game(10, 10);
-newGame.createPlayers();
-console.log(newGame.activePlayers);
